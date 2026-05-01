9e040e941201779071c23b7fda14122c408b9c40
Ramkrithik S <67008656+ramkrithik@users.noreply.github.com>
2026-05-01 15:02:10 +0530

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        stack = [root]
+        visited = [False]
+        val =[]
+
+        while stack:
+            curr, visit = stack.pop(), visited.pop()
+            if curr:
+                if visit:
+                    val.append(curr.val)
+                else:
+                    stack.append(curr)
+                    visited.append(True)
+                    stack.append(curr.right)
+                    visited.append(False)
+                    stack.append(curr.left)
+                    visited.append(False)
+        
+        return val
