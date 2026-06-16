d2305a2a5e10ec7238271deb3e653cad69d5c525
Ramkrithik S <67008656+ramkrithik@users.noreply.github.com>
2026-06-16 08:40:49 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+        curcomb, allcomb = [], []
+        self.helper(1,curcomb,allcomb,n,k)
+        return allcomb
+
+    
+    def helper(self, i, curcomb,allcomb,n,k):
+        if len(curcomb) == k:
+            allcomb.append(curcomb.copy())
+            return
+        if i > n:
+            return
+        
+        curcomb.append(i)
+        self.helper(i+1,curcomb,allcomb,n,k)
+        curcomb.pop()
+
+        self.helper(i+1,curcomb,allcomb,n,k)
+
+        
